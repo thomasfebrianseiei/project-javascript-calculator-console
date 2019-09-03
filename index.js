704099e6558bbe058b4d580147f70a59d4df3ccc
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2019-09-03 15:28:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const digitA = 2000
+const digitB = 200.55
+
+const resultAddition = digitA + digitB
+const resultSubstraction = digitA - digitB
+const resultMultiplication = digitA * digitB
+const resultDivison = digitA / digitB
+
+console.log(digitA)
+console.log(digitB)
+
+console.log(resultAddition)
+console.log(resultSubstraction)
+console.log(resultMultiplication)
+console.log(resultDivison)
